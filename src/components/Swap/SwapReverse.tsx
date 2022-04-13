19a57df3a45d051c8bb54a4714f720536ae26bb9
subject026 <subject026@protonmail.com>
2022-04-13 16:18:34 +0100

    tweaked icon colors

--- src/components/Swap/SwapReverse.tsx
+++ src/components/Swap/SwapReverse.tsx
@@ -7,13 +7,13 @@ type Props = {
 const SwapReverse: React.FC<Props> = (props) => {
   return (
     <button
-      className="inline-block my-4 p-3 rounded-full bg-breadgray-100"
+      className="inline-block my-4 p-3 rounded-full bg-breadgray-100 text-neutral-600 hover:text-neutral-500"
       {...props}
     >
       <svg className="w-7 h-7" fill="none" viewBox="0 0 123 105">
-        <path className="fill-current text-neutral-700" d="M37 0h50v60H37z" />
+        <path className="fill-current" d="M37 0h50v60H37z" />
         <path
-          className="fill-current text-neutral-700"
+          className="fill-current "
           d="M61.5 105 8.24 45H114.76L61.5 105Z"
         />
       </svg>
