872f4b5d9bbbc80d7756a0da84b67f4d04bd11a8
subject026 <subject026@protonmail.com>
2022-04-24 17:55:54 +0100

    layout fix

--- src/components/WalletDisplay/WalletDisplay.tsx
+++ src/components/WalletDisplay/WalletDisplay.tsx
@@ -5,7 +5,7 @@ const WalletDisplay: React.FC = (props) => {
   const { children } = props;
 
   return (
-    <span className="relative text-xs w-full flex justify-center md:justify-right mt-4 truncate text-ellipsis">
+    <span className="text-xs w-full flex justify-center md:justify-end mt-4 truncate text-ellipsis">
       <TextTransition>{children}</TextTransition>
     </span>
   );
