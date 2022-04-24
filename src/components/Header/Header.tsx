4372298e7b4b4c5cc386b1451c16d5cb66cc5203
subject026 <subject026@protonmail.com>
2022-04-24 16:31:38 +0100

    adjust logo size & header padding

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header: React.FC = ({ children }) => {
   return (
     <header className="bg-breadgray-100">
-      <div className="max-w-6xl m-0 mx-auto px-2 py-6 md:p-4 flex md:grid md:grid-cols-3">
+      <div className="max-w-6xl m-0 mx-auto px-4 py-4 md:p-4 flex md:grid md:grid-cols-3">
         {children}
       </div>
     </header>
