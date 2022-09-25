c87611c24ae9bfbf161f5a2770a715da690d9bbe
subject026 <subject026@protonmail.com>
2022-09-26 00:18:27 +0100

    add initial button story to test

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,40 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import Button from "./Button";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Button",
+  component: Button,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  //   argTypes: {
+  //     backgroundColor: { control: "color" },
+  //   },
+} as ComponentMeta<typeof Button>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Regular = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Regular.args = {
+  children: "Default",
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  children: "Button",
+  disabled: true,
+};
+
+// export const Large = Template.bind({});
+// Large.args = {
+//   size: "large",
+//   label: "Button",
+// };
+
+// export const Small = Template.bind({});
+// Small.args = {
+//   size: "small",
+//   label: "Button",
+// };
