6afd8988452466a10e0ab0f9c5a31c7b3bd01b1b
subject026 <subject026@protonmail.com>
2022-10-22 10:10:33 +0100

    clean up config

--- synpress.config.ts
+++ synpress.config.ts
@@ -2,35 +2,24 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   userAgent: "synpress",
-
   retries: {
     runMode: 0,
     openMode: 0,
   },
-
   screenshotsFolder: "screenshots",
   videosFolder: "videos",
   video: true,
   chromeWebSecurity: true,
   viewportWidth: 1366,
   viewportHeight: 850,
-
   env: {
     coverage: false,
   },
-
   defaultCommandTimeout: 30000,
   pageLoadTimeout: 30000,
   requestTimeout: 30000,
-
-  component: {
-    setupNodeEvents(on, config) {},
-    specPattern: "./**/*spec.{js,jsx,ts,tsx}",
-  },
-
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+  // component: {
+  //   setupNodeEvents(on, config) {},
+  //   specPattern: "./**/*spec.{js,jsx,ts,tsx}",
+  // },
 });
