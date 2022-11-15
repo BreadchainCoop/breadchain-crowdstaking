32081727d705e61149ceda78900d4bf30f6149c7
subject026 <subject026@protonmail.com>
2022-11-15 17:51:03 +0000

    refactor wallet display and mob nav toggle

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,29 +1,41 @@
-import { useAccount } from 'wagmi';
+import { ReactNode } from 'react';
+import { Chain, useAccount, useNetwork } from 'wagmi';
 import { DesktopNavigation } from '../Navigation';
 import Logo from './Logo';
-import * as WalletDisplay from './WalletDisplay';
-import { formatAddress } from '../../util';
+import WalletDisplay from './WalletDisplay';
+
 import MobileNavigationToggle from './MobileNavigationToggle';
+
+function Container({ children }: { children: ReactNode }) {
+  return (
+    <header className="bg-breadgray-100">
+      <div className="max-w-6xl m-0 mx-auto px-6 py-4 md:py-5 md:px-8 flex justify-between">
+        {children}
+      </div>
+    </header>
+  );
+}
+
+const getChainString = (chain: (Chain & {
+  unsupported?: boolean | undefined;
+}) | undefined) => {
+  if (chain === undefined) return 'not connected';
+  if (chain.unsupported) return 'unsupported';
+  return chain.name;
+};
 
 function Header() {
   const { address: accountAddress } = useAccount();
+  const { chain } = useNetwork();
 
   return (
-    <header className="bg-breadgray-100">
-      <div className="max-w-6xl m-0 mx-auto p-6 md:px-8 flex h-24">
-        <Logo />
-        <DesktopNavigation />
-        <WalletDisplay.Container>
-          <WalletDisplay.Network />
-          {accountAddress && (
-            <WalletDisplay.Address>
-              {formatAddress(accountAddress)}
-            </WalletDisplay.Address>
-          )}
-        </WalletDisplay.Container>
-        <MobileNavigationToggle />
-      </div>
-    </header>
+    <Container>
+      <Logo />
+      <DesktopNavigation />
+      <WalletDisplay accountAddress={accountAddress} chainString={getChainString(chain)} />
+      <MobileNavigationToggle />
+    </Container>
+
   );
 }
 
--- src/components/Header/MobileNavigationToggle.tsx
+++ src/components/Header/MobileNavigationToggle.tsx
@@ -1,12 +1,10 @@
 function MobileNavigationToggle() {
   return (
-    <div className="flex justify-end md:hidden">
-      <button type="button" className="text-neutral-600 p-1 h-10 w-10 fill">
-        <svg className="fill-current" viewBox="0 0 24 24">
-          <path d="M3 18h18v-2H3v2Zm0-5h18v-2H3v2Zm0-7v2h18V6H3Z" />
-        </svg>
-      </button>
-    </div>
+    <button type="button" className="text-neutral-600 p-1 h-10 w-10 fill md:hidden">
+      <svg className="fill-current" viewBox="0 0 24 24">
+        <path d="M3 18h18v-2H3v2Zm0-5h18v-2H3v2Zm0-7v2h18V6H3Z" />
+      </svg>
+    </button>
   );
 }
 
