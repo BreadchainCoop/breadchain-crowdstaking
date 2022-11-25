7a5b2cad7e00efd7e5afbc076ad43c7017bba911
subject026 <subject026@protonmail.com>
2022-11-25 14:33:10 +0000

    rename ui components

--- src/components/Swap/TokenDisplay.tsx
+++ src/components/Swap/TokenDisplay.tsx
@@ -1,15 +1,15 @@
 import { ReactNode } from 'react';
 
-function Header({ children }: { children: ReactNode }) { return <div className="mb-6 sm:mb-8">{children}</div>; }
+export function PanelHeader({ children }: { children: ReactNode }) { return <div className="mb-6 sm:mb-8">{children}</div>; }
 
-function Content({ children }: { children: ReactNode }) { return <div className="flex items-center">{children}</div>; }
+export function PanelContent({ children }: { children: ReactNode }) { return <div className="flex items-center">{children}</div>; }
 
 type TBalanceProps = {
   children: ReactNode
   onClick: () => void;
 };
 
-function BalanceButton({ onClick, children }: TBalanceProps) {
+export function PanelBalanceButton({ onClick, children }: TBalanceProps) {
   return (
     <button
       type="button"
@@ -21,7 +21,7 @@ function BalanceButton({ onClick, children }: TBalanceProps) {
   );
 }
 
-function Balance({ children }: { children: ReactNode }) {
+export function PanelBalance({ children }: { children: ReactNode }) {
   return (
     <span className="text-xs sm:text-sm pb-3 inline-block">
       {children}
@@ -29,14 +29,10 @@ function Balance({ children }: { children: ReactNode }) {
   );
 }
 
-function Container({ children }: { children: ReactNode }) {
+export function PanelContainer({ children }: { children: ReactNode }) {
   return (
     <div className="bg-breadgray-100 text-gray-300 w-full px-6 py-4 sm:px-8 sm:py-6">
       {children}
     </div>
   );
 }
-
-export default {
-  Container, Balance, BalanceButton, Content, Header,
-};
