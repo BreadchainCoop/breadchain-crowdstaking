d825886772f9f0ae42d2c872ac09c27543a9e5aa
subject026 <subject026@protonmail.com>
2022-11-27 17:47:31 +0000

    implement bake/burn tx with wagmi hooks

--- src/components/Swap/BakeOrBurn/BakeOrBurn.tsx
+++ src/components/Swap/BakeOrBurn/BakeOrBurn.tsx
@@ -1,21 +1,61 @@
+import { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
+import { useContractWrite, usePrepareContractWrite } from 'wagmi';
+
+import { parseEther } from 'ethers/lib/utils.js';
 import Button from '../../Button';
 
+import type { ChainConfiguration } from '../../../config';
+
+import BREADABI from '../../../BreadPolygon.json';
+import useDebounce from '../../../hooks/useDebounce';
+
+const { abi } = BREADABI;
 interface IProps {
   mode: 'BAKE' | 'BURN',
-  value: string
+  value: string,
+  accountAddress: string,
+  chainConfig: ChainConfiguration
 }
 
-function BakeOrBurn({ mode, value }: IProps) {
-  const handleSubmit = async () => {
+function BakeOrBurn({
+  mode, value, chainConfig, accountAddress,
+}: IProps) {
+  const [approvalTx, setapprovalTx] = useState<null | `0x${string}`>(null);
 
+  const debouncedValue = useDebounce(value, 500);
+
+  const { BREAD } = chainConfig;
+
+  const parsedValue = parseEther(debouncedValue || '0');
+
+  const { config } = usePrepareContractWrite({
+    address: BREAD.address,
+    abi,
+    functionName: mode === 'BAKE' ? 'mint' : 'burn',
+    args: [parsedValue, accountAddress],
+  });
+
+  const {
+    data, isLoading, isSuccess, write,
+  } = useContractWrite(config);
+
+  useEffect(() => {
+    if (isSuccess && data) {
+      setapprovalTx(data.hash);
+    }
+  }, [isSuccess, data]);
+
+  const handleSubmit = async () => {
+    write?.();
   };
 
   return (
     <Button
       onClick={handleSubmit}
       disabled={
-        transaction?.status === 'PENDING'
-        || parseFloat(value) === 0
+        // transaction?.status === 'PENDING'
+        parseFloat(value) === 0
         || value === ''
 
       }
