123add3d2b32b517fb2ed667f89e57778d9ac0dc
subject026 <subject026@protonmail.com>
2022-12-04 17:15:03 +0000

    reconnect modal & send error to toast

--- src/components/Swap/ApproveContract/ApproveContract.tsx
+++ src/components/Swap/ApproveContract/ApproveContract.tsx
@@ -6,12 +6,16 @@ import Button from '../../Button';
 
 import ERC20ABI from '../../../ERC20.json';
 import type { ChainConfiguration } from '../../../config';
+import { useModal } from '../../../context/ModalContext';
+import { useToast } from '../../../context/ToastContext';
 
 interface IProps {
   chainConfig: ChainConfiguration;
 }
 
 function ApproveContract({ chainConfig }: IProps) {
+  const { dispatch: dispatchModal } = useModal();
+  const { dispatch: dispatchToast } = useToast();
   const [approvalTx, setapprovalTx] = useState<null | `0x${string}`>(null);
 
   const { DAI, BREAD } = chainConfig;
@@ -24,16 +28,29 @@ function ApproveContract({ chainConfig }: IProps) {
   });
 
   const {
-    data, isLoading, isSuccess, write,
+    data, error, isLoading, isSuccess, write,
   } = useContractWrite(config);
 
   useEffect(() => {
+    if (error) {
+      console.log({ error });
+      dispatchModal({ type: 'CLEAR_MODAL' });
+      dispatchToast({
+        type: 'SET_TOAST',
+        payload: {
+          type: 'ERROR',
+          message: 'User rejected transaction',
+        },
+      });
+    }
     if (isSuccess && data) {
+      dispatchModal({ type: 'CLEAR_MODAL' });
       setapprovalTx(data.hash);
     }
-  }, [isSuccess, data]);
+  }, [isSuccess, data, error]);
 
   const handleApproveContract = async () => {
+    dispatchModal({ type: 'SET_MODAL', payload: { type: 'APPROVAL', title: 'Approving BREAD Contract' } });
     write?.();
   };
 
