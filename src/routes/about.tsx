ec426b8049ff0bcae7711b50a065e767cd73567e
subject026 <subject026@protonmail.com>
2023-01-23 17:43:24 +0000

    increase body font size for about page

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -3,7 +3,8 @@ import { html } from '../markdown/about.md';
 function About() {
   return (
     <section
-      className="prose prose-pink font-redhat md:prose md:prose-pink prose-invert m-auto px-4 pb-16 sm:px-4 md:px-6"
+      className="m-auto px-4 pb-16 sm:px-4 md:px-6 prose prose-pink text-[18px] prose-headings:font-pressstart
+       font-redhat md:prose md:prose-pink prose-invert"
       /* eslint-disable-next-line */
       dangerouslySetInnerHTML={{ __html: html }}
     />
