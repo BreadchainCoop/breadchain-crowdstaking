d644282e148d1b6f6eac7ff7225315765faca05d
subject026 <subject026@protonmail.com>
2023-01-29 12:03:13 +0000

    about page text styles

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,12 +1,10 @@
 import { html } from '../markdown/about.md';
 
 function About() {
-  //
-  // COMMENT what is this?
   return (
     <section
       className="prose prose-invert prose-pink m-auto px-4 pb-16 font-redhat text-[18px] prose-headings:font-pressstart
-       sm:px-4 md:px-6 md:prose md:prose-pink"
+       sm:px-4 md:px-6 md:prose"
       /* eslint-disable-next-line */
       dangerouslySetInnerHTML={{ __html: html }}
     />
