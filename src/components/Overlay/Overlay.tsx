c37c8db4730c0b51d48fc6a6fd9a0ebdf8f29141
subject026 <subject026@protonmail.com>
2023-01-29 11:49:05 +0000

    seperate overlay

---
+++ src/components/Overlay/Overlay.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from 'react';
+import { classNames } from '../../util';
+
+function Overlay({
+  isOpen,
+  children,
+}: {
+  isOpen: boolean;
+  children?: ReactNode;
+}) {
+  return (
+    <div
+      className={classNames(
+        'z-9 fixed top-0 bottom-0 left-0 right-0 h-screen w-screen bg-neutral-900 opacity-0 transition-opacity',
+        isOpen ? 'block opacity-70' : 'hidden',
+      )}
+    >
+      {children !== null && children}
+    </div>
+  );
+}
+
+Overlay.defaultProps = {
+  children: null,
+};
+export default Overlay;
